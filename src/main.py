70b5d0575de2816859bc5cc78386661227d65641
glbondiii <glbondiii@protonmail.com>
2023-12-30 21:21:35 -0600

    Prep for HTTP server

--- src/main.py
+++ src/main.py
@@ -5,13 +5,18 @@ from question import Question
 
 def main():
     print("ExamPort for StudyBinder (into SQL format)\n")
+    isScript: bool = True
     path_to_databases: str = input("Enter the absolute path where your databases are: ")
     courseID: str = input("Enter the course ID for your exam: ")
     db: sqlite3.Connection = sqldb.connectToDatabase(path_to_databases, courseID)
-    questions: list[Question] = script.getExamQuestions()
-    sqldb.addQuestionsToDatabase(questions, db)
-    if db:
-        db.close()
+    if db: 
+        if isScript:
+            questions: list[Question] = script.getExamQuestions()
+            sqldb.addQuestionsToDatabase(questions, db)
+            db.close()
+        else:
+            # Start HTTP Server here
+            pass
 
 if __name__ == "__main__":
     main()
