413ce135e3091a209333dde79ea41458f10762af
glbondiii <glbondiii@protonmail.com>
2024-01-04 23:20:32 -0600

    Path is set once per server runtime

--- src/server.py
+++ src/server.py
@@ -1,20 +1,22 @@
-import atexit
 import sqlite3
 import sqldb
 from flask import Flask, jsonify
 from question import Question
 
 app: Flask = Flask(__name__)
+path: str = ""
 db: sqlite3.Connection
 
 @app.route("/")
 @app.route("/init")
 @app.route("/init/<courseID>")
 def init(courseID: str = ""):
+    global path
     global db
+    if (path == ""):
+        path = input("Enter the absolute path where your databases are: ")
     if (courseID == ""):
         return "Enter the course ID for your exam in the URL (e.g localhost:5000/init/[courseID])"
-    path = input("Enter the absolute path where your databases are: ")
     db = sqldb.connectToDatabase(path, courseID)
     return f"Set database to {courseID} at {path}"
 
